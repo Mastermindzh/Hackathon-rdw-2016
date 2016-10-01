4b5a8cd3c33fbf9f3d9f58d44bb8788d16e030a2
ijsklontje <pascalrejaan@hotmail.com>
2016-10-02 01:52:02 +0200

    some gets and a post

--- angular/src/js/controllers/cars-edit-ctrl.js
+++ angular/src/js/controllers/cars-edit-ctrl.js
@@ -1,8 +1,31 @@
 
-app.controller('CarEditCtrl', ['$scope','$http','$interval','Config','Alert', CarEditCtrl]);
+app.controller('CarEditCtrl', ['$scope','$stateParams', '$http','$interval','Config','Alert', '$window', CarEditCtrl]);
 
-    function CarEditCtrl($scope, $http, $interval, Config, Alert) {
+    function CarEditCtrl($scope, $stateParams,$http, $interval, Config, Alert, $window) {
+        console.log($stateParams.id);
 
+
+        $http.get(Config.url + '/cars/' + $stateParams.id)
+            .success(function(data, status, headers, config) {
+                $scope.car =  data;
+            })
+            .error(function(data, status, headers, config) {
+                Alert.addAlert("danger","fa-exclamation-triangle","Can't fetch car data");
+            });
+        $scope.addData = function(inputveld) {
+            $http({
+                url: Config.url + '/maintenance/add',
+                method: "POST",
+                data: { 'message' : inputveld }
+            })
+                .then(function(response) {
+                        $window.location.href = '#/cars/';
+
+                    },
+                    function(response) { // optional
+                        console.log("faal");
+                    });
+        }
     }
 
 
